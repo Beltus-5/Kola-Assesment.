d32f898c4b9e2ce5c96682ed26c834cfcbee7ac3
unknown <beltustahbod@gmail.com>
2024-11-26 09:26:23 +0100

    Last name test cases

--- tests/registration-page-test.spec.js
+++ tests/registration-page-test.spec.js
@@ -76,4 +76,71 @@ test.describe("Registration Page Test", () => {
     expect(locators.firstNameError).toContainText("Please fill in this field");
   });
 
+   // ============================================Last Name==============================================================
+   test("Verify Form Correctly Validates Profile Creation With Valid Alphabetic Characters Filled in Last Name Field", async ({
+    page,
+  }) => {
+    const locators = await fieldsLocators(page); //Create object of the fieldsLocators
+    //verify the last name input field is enabled
+    await expect(locators.firstName).toBeEnabled();
+
+    //Enters Last name John in the input box
+    await locators.firstName.fill(user.last_name[0]);
+
+    await locators.submitButton.click();
+
+    //Asserts John is displayed in the input box after filling
+    expect(locators.firstName).toHaveValue(user.last_name[0]);
+  });
+
+  test("Verify Last Name field correctly rejects special characters", async ({
+    page,
+  }) => {
+    const locators = await fieldsLocators(page);
+    //Enters Invalid last name
+    await locators.lastName.fill(user.last_name[1]);
+    await locators.submitButton.click();
+    expect(locators.lastNameError).toContainText(
+      "This field must contain only letters"
+    );
+  });
+  test("Verify Last Name field correctly rejects Alphanumeric Characters", async ({
+    page,
+  }) => {
+    const locators = await fieldsLocators(page);
+    //Enters Invalid last name
+    await locators.lastName.fill(user.last_name[2]);
+    await locators.submitButton.click();
+    expect(locators.lastNameError).toContainText(
+      "This field must contain only letters"
+    );
+  });
+
+  test("Verify Last Name Field correctly rejects white Spaces Between Letters", async ({
+    page,
+  }) => {
+    const locators = await fieldsLocators(page);
+
+    //Enters Invalid first name
+    await locators.lastName.fill(user.last_name[3]);
+    await locators.submitButton.click();
+    expect(locators.lastNameError).toContainText(
+      "This field's entry must not have space characters"
+    );
+  });
+
+  test("Verify Last Name Field throws an error if left empty", async ({
+    page,
+  }) => {
+    const locators = await fieldsLocators(page);
+    //verify the first name input field is enabled
+    await expect(locators.lastName).toBeEnabled();
+
+    //Enters Invalid first name
+    await locators.lastName.fill(user.first_name[4]);
+
+    await locators.submitButton.click();
+    expect(locators.lastNameError).toContainText("Please fill in this field");
+  });
+
 })
