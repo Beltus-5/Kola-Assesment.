1bfe41861acaccd9de6ca0c3438064fc2e651444
unknown <beltustahbod@gmail.com>
2024-11-26 09:17:46 +0100

    Created fillOptionalFields function

--- PageObject/RegistrationPagePOM.js
+++ PageObject/RegistrationPagePOM.js
@@ -29,3 +29,14 @@ async function fillMandatoryFields(page) {
   
   }
   
+  
+async function fillOptionalFields(page) {
+  
+    await page.check('input#gender1');
+    await page.fill('input#date-of-birth', user.date_Of_birth[0]);
+    await page.locator('input#phone-number').fill(user.phone_number[0]);
+    await page.locator('input[name="address"]').fill(user.address_[0]);
+    await page.locator('input[name="linkedin-url"]').fill(user.linkedin_url[0]);
+    await page.locator('input[name="github-url"]').fill(user.github_url[0]);
+   
+  }
