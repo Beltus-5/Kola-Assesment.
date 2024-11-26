e85418d109e21b087bc40c2fb89de736bc8c6733
unknown <beltustahbod@gmail.com>
2024-11-26 09:31:46 +0100

    Linkedin testcases

--- tests/registration-page-test.spec.js
+++ tests/registration-page-test.spec.js
@@ -470,4 +470,29 @@ test.describe("Registration Page Test", () => {
     await locators.submitButton.click();
   });
 
+   // ============================================Linkedin==============================================================
+   test("Verify Linkedin Field Correctly Validates With Valid input", async ({
+    page,
+  }) => {
+    const locators = await fieldsLocators(page);
+
+    // Fill the address field with invalid input
+    await locators.linkedinUrl.fill(user.linkedin_url[0]);
+
+    page.on("dialog", async (dialog) => {
+      dialogHandled = true;
+      try {
+        // Assert the alert message
+        expect(dialog.message()).toBe("First name must be filled out"); //verification to assert Linkedinvalue was accepted and next required input needed
+        await dialog.accept();
+      } catch (error) {
+        console.error("Error handling the dialog:", error);
+        await dialog.dismiss();
+      }
+    });
+
+    //Submits the form
+    await locators.submitButton.click();
+  });
+
 })
